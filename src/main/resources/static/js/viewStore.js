04db8ee26ca8cef9af3a03e4f0751b4537feaeca
kitaee <dlrlxo999@naver.com>
2021-11-20 05:25:32 +0900

    test

---
+++ src/main/resources/static/js/viewStore.js
@@ -0,0 +1,26 @@
+function Two (target){
+
+    target.style.left=target.getAttribute('data-left')+'px';
+    target.style.top=target.getAttribute('data-top')+'px';
+}
+
+function Four (target){
+    target.style.left=target.getAttribute('data-left')+'px';
+    target.style.top=target.getAttribute('data-top')+'px';
+}
+function Counter (target){
+    target.style.left=target.getAttribute('data-left')+'px';
+    target.style.top=target.getAttribute('data-top')+'px';
+}function Toilet (target){
+    target.style.left=target.getAttribute('data-left')+'px';
+    target.style.top=target.getAttribute('data-top')+'px';
+}function Now (target){
+    target.style.left=target.getAttribute('data-left')+'px';
+    target.style.top=target.getAttribute('data-top')+'px';
+}
+function Entrance (target){
+    target.style.left=target.getAttribute('data-left')+'px';
+    target.style.top=target.getAttribute('data-top')+'px';
+}
+
+
